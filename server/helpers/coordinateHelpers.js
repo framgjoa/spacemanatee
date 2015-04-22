b9fabd2b42232f519f5367ee81e81099861f0121
Ben Pevsner <pevsner.ben@gmail.com>
2015-04-21 19:28:27 -0700

    mend

--- server/helpers/coordinateHelpers.js
+++ server/helpers/coordinateHelpers.js
@@ -2,29 +2,29 @@
 if (typeof(Number.prototype.toRad) === "undefined") {
   Number.prototype.toRad = function() {
     return this * Math.PI / 180;
-  }
+  };
 }
 
 // calculate the distance between 2 waypoints, given their latitudes and longitudes, return distance in miles
 module.exports.calcDistance = function(pt1, pt2) {
   var R = 6371; // earth radius, in km
-  var lat1 = pt1.location.coordinate['latitude'];
-  var lon1 = pt1.location.coordinate['longitude'];
-  var lat2 = pt2.location.coordinate['latitude'];
-  var lon2 = pt2.location.coordinate['longitude'];
+  var lat1 = pt1.location.coordinate.latitude;
+  var lon1 = pt1.location.coordinate.longitude;
+  var lat2 = pt2.location.coordinate.latitude;
+  var lon2 = pt2.location.coordinate.longitude;
 
 
   var dLat = (lat2 - lat1).toRad();
   var dLon = (lon2 - lon1).toRad();
-  var lat1 = lat1.toRad();
-  var lat2 = lat2.toRad();
+  lat1 = lat1.toRad();
+  lat2 = lat2.toRad();
 
   var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
           Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   var distance = R * c * 0.621371; // convert distance from km to miles
   return distance;
-}
+};
 
 // parse google coordinate into {latitude:..., longitude: ... } format
 module.exports.parseGoogleCoord = function(googleCoord) {
@@ -37,9 +37,9 @@ module.exports.parseGoogleCoord = function(googleCoord) {
         longitude: longitude
       }
     }
-  }
+  };
   return obj;
-}
+};
 
 // trim the google waypoint coordinate to take out start and end way point so no clustering at 2 ends.
 module.exports.trimGoogleCoord = function(googleCoords, distance) {
@@ -56,4 +56,4 @@ module.exports.trimGoogleCoord = function(googleCoords, distance) {
     trimmedCoords = googleCoords;
   }
   return trimmedCoords;
-}
+};
